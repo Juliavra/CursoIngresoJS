4a8c31acdf6a195396e704eb44ab66faae685c97
Juliavra <40763767+Juliavra@users.noreply.github.com>
2018-07-05 19:44:48 -0300

    Ejercicio resuelto usando Prompt & get element by ID

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+
+
+  //SOLUCION USANDO PROMPT
+
+  //nombre=prompt ("ingrese su nombre", "coloque su nombre aqui");
+  //localidad=prompt ("ingrese su localidad", "coloque su localidad aqui");
+  //alert ("Usted es "+nombre+" y vive en la localidad de "+ localidad);
+
+  //SOLUCION USANDO getElementById
+
+  //nombre=document.getElementById('elNombre').value;
+  //localidad=document.getElementById('laLocalidad').value;
+  //alert ("Usted es "+nombre+" y vive en la localidad de "+ localidad);
+
 }
