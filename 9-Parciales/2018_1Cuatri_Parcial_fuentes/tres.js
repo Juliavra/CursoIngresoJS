2c0f1843a2ae6d4a8b377821f8af888a10ab41a0
Juliavra <40763767+Juliavra@users.noreply.github.com>
2018-07-05 20:01:52 -0300

    Chequear q get element by ID este bien utilizado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+  var precio;
+  var descuento;
+  var precioFinal;
 
+  precio=prompt ("ingrese el precio", "coloque el precio aqui");
+  descuento=prompt ("ingrese el descuento", "coloque el % de descuento aqui");
+
+  precioFinal= precio-[(precio*descuento)/100];
+
+//document.getElementById('elPrecioFinal').value=precioFinal;
+
+//console.log (precioFinal);
 }
