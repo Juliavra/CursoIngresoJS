c3c079edb02730dbb430e267c8058d87d93e2bc2
Juliavra <40763767+Juliavra@users.noreply.github.com>
2018-07-05 19:24:02 -0300

    ejercicio Resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var perimetro;
 
+ancho=prompt ("ingrese el ancho del rectangulo", "ancho");
+largo=prompt ("ingrese el largo del rectangulo", "largo");
+
+perimetro = ancho*2+largo*2;
+
+alert ("el perimetro es: "+perimetro);
 }
