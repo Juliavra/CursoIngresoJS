42978f1aed40a2d99dfa7da43a1e6fb8e50a8d91
Juliavra <40763767+Juliavra@users.noreply.github.com>
2018-07-06 11:55:57 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,19 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var sueldo;
+	var resultado;
+	var aumento;
+	var porcentaje;
+
 	
+
+	sueldo=parseInt (sueldo);
+	porcentaje=10;
+	aumento=(sueldo*porcentaje)/100;
+
+	resultado=aumento+sueldo;
+	
+	alert ("El resultado es: "+resto);
+
 }
