442df80886ec52e72d3e4e507f40d0da6571694b
Juliavra <40763767+Juliavra@users.noreply.github.com>
2018-07-12 19:32:29 -0300

    Update jsIteraciones (3).js

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,12 @@
+//al presionar el botón pedir la CLAVE (ayuda: es utn750)
+
 function mostrar()
 {
+  var clave;
 
-var clave = prompt("ingrese el número clave.");
+  while (clave!="utn750") {
 
+         clave= prompt ("ingrese su clave:");}
 
+   alert ("Ud ingreso: "+clave+" Su clave es correcta!!!");
 }//FIN DE LA FUNCIÓN
