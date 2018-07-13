16da3fec451e0f4e2fb11e810fcf39b188ff431f
Juliavra <40763767+Juliavra@users.noreply.github.com>
2018-07-13 12:06:39 -0300

    Switch con Codigo reducido (casos agrupados

---
+++ 5-InstruccionWhile/jsInstruccionSwitch(5).js
@@ -0,0 +1,23 @@
+/*Al ingresar una hora, informar: 
+si está entre las 7 y las 11 : "Es de mañana.".
+*/
+
+
+function mostrar()
+{
+
+var laHora = document.getElementById('hora').value;
+laHora= parseInt (laHora);
+
+switch (laHora) {
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		alert ("Es de mañana.");
+			break;
+		default:
+		alert ("No Es de mañana.");
+		}
+}//FIN DE LA FUNCIÓN
